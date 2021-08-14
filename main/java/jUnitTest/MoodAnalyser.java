841911d64b477a5cecfe4f9a8cb32ac7631cc36e
KratiGupta <kratigupta8545@gmail.com>
2021-08-14 16:05:59 +0530

    Added Welcome Message

---
+++ main/java/jUnitTest/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package jUnitTest;
+public class MoodAnalyser {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to master branch of Mood Analyser");
+    }
+    }
